1090473d91a3ac06442e80abc3eb4c3521787e1e
AbhishekD879 <abhishekdiwate879@gmail.com>
2022-05-27 14:23:16 +0530

    tensorLimit

--- _03_facenet_keras/face_recognizer_image.py
+++ _03_facenet_keras/face_recognizer_image.py
@@ -10,7 +10,22 @@ import pickle
 import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import Normalizer
+from keras import backend as K
 
+num_cores = 2
+
+num_CPU = 1
+num_GPU = 0
+
+config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,
+                        inter_op_parallelism_threads=num_cores, 
+                        allow_soft_placement=True,
+                        device_count = {'CPU' : num_CPU,
+                                        'GPU' : num_GPU}
+                       )
+
+session = tf.Session(config=config)
+K.set_session(session)
 # temp
 def get_encode(face_encoder, face, size):
     face = normalize(face)
--- _03_facenet_keras/prepare_data.py
+++ _03_facenet_keras/prepare_data.py
@@ -8,7 +8,22 @@ from keras.models import load_model
 import matplotlib.pyplot as plt
 import cv2
 from sklearn.preprocessing import Normalizer
+from keras import backend as K
 
+num_cores = 2
+
+num_CPU = 1
+num_GPU = 0
+
+config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,
+                        inter_op_parallelism_threads=num_cores, 
+                        allow_soft_placement=True,
+                        device_count = {'CPU' : num_CPU,
+                                        'GPU' : num_GPU}
+                       )
+
+session = tf.Session(config=config)
+K.set_session(session)
 # from utils import get_face, get_encode, l2_normalizer, normalize
 
 # temp
